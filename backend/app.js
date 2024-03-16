fd4f186676e025b3234ffb9e523a61942c068226
Damian <d.tarczon@mits.pl>
2024-03-16 15:25:42 +0100

    backend changed frontend url

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:4200",
+  origin: "https://main--golden-custard-f1bea7.netlify.app",
   optionsSuccessStatus: 204,
   methods: "GET, POST, PUT, DELETE",
 };
